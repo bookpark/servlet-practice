83331409421d52a2fcdfba1129a6d949abc9f521
Booki Park <bkbkgg@gmail.com>
2023-05-14 20:00:58 +0900

    톰캣 실행 오류 수정

--- src/main/java/org/example/WebApplicationServerLauncher.java
+++ src/main/java/org/example/WebApplicationServerLauncher.java
@@ -13,7 +13,7 @@ public class WebApplicationServerLauncher {
 
     public static void main(String[] args) throws LifecycleException {
         // 내장 톰캣
-        String webappDirLocation = "webapp/";
+        String webappDirLocation = "webapps/";
         Tomcat tomcat = new Tomcat();
         tomcat.setPort(8080);
 
